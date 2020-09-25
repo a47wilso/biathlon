b7ab02609bf49596fe79e51abd4fdbf4f3bffe0a
a47wilso <71887153+a47wilso@users.noreply.github.com>
2020-09-25 10:37:04 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -122,21 +122,21 @@ scene.setBackgroundImage(img`
     `)
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
+    . . . . . . . 2 2 . . . . . . . 
+    . . . . . . 2 2 2 2 . . . . . . 
+    . . . . . . 2 d d 2 . . . . . . 
+    . . . . . . d d d d . . . . . . 
+    . . . f . . . d d . . . f . . . 
+    . . . . f f 8 8 8 8 f f . . . . 
+    . . . . . 8 8 8 8 8 8 . . . . . 
+    . . . . a . 8 8 8 8 . a . . . . 
+    . . . . a . 8 8 8 8 . a . . . . 
+    . . . . a a f 8 8 f a a . . . . 
+    . . . . . a f f f f a . . . . . 
+    . . . . . a f . . f a . . . . . 
+    . . . . . . a . . a . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . b 5 5 b . . . 
-    . . . . . . b b b b b b . . . . 
-    . . . . . b b 5 5 5 5 5 b . . . 
-    . b b b b b 5 5 5 5 5 5 5 b . . 
-    . b d 5 b 5 5 5 5 5 5 5 5 b . . 
-    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
-    . . b d 5 5 b 1 f f 5 4 4 c . . 
-    b b d b 5 5 5 d f b 4 4 4 4 b . 
-    b d d c d 5 5 b 5 4 4 4 4 4 4 b 
-    c d d d c c b 5 5 5 5 5 5 5 b . 
-    c b d d d d d 5 5 5 5 5 5 5 b . 
-    . c d d d d d d 5 5 5 5 5 d b . 
-    . . c b d d d d d 5 5 5 b b . . 
-    . . . c c c c c c c c b b . . . 
+    . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 mySprite.setFlag(SpriteFlag.StayInScreen, true)
 controller.moveSprite(mySprite, 200, 200)
